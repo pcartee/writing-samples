30b679f15b50db48d522ede4662455479a9579ea
pcartee <pcartee@users.noreply.github.com>
2026-08-07 15:45:21 +0000

    Deploy preview for PR 18 🛫

---
+++ pr-preview/pr-18/assets/js/5e95c892.21cb1619.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunksample_writing=globalThis.webpackChunksample_writing||[]).push([[9647],{7121(e,s,r){r.r(s),r.d(s,{default:()=>c});r(6540);var a=r(4164),i=r(7559),l=r(5500),n=r(2831),t=r(1656),u=r(4848);function c(e){return(0,u.jsx)(l.e3,{className:(0,a.A)(i.G.wrapper.docsPages),children:(0,u.jsx)(t.A,{children:(0,n.v)(e.route.routes)})})}}}]);
